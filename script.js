dc409ada32f4ea348b0093723e425cf1429a6ab5
DeividBorges93 <deividmborges@gmail.com>
2023-10-03 16:32:24 -0300

    feat(script.js): :recycle: adiciona saveAPicture no script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import colorSelection from "./app/paleta_de_cores/colorSelection.js";
 import paintAPixel from "./app/tela_em_branco/paintAPixel.js";
 import cleanTheCanvas from "./app/tela_em_branco/cleanTheCanvas.js";
 import clearAPixel from "./app/tela_em_branco/clearAPixel.js";
+import saveAPicture from "./app/tela_em_branco/saveAPicture.js";
 
 window.onload = pixelArts;
 
@@ -14,4 +15,5 @@ function pixelArts() {
   paintAPixel();
   cleanTheCanvas();
   clearAPixel();
+  saveAPicture();
 };
